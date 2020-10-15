6a04a0529ec029f573497419f6bbe774ee9af1c3
MyBoyDenis <shelbykohring@Shelbys-MacBook-Pro.local>
2020-10-15 15:50:57 -0500

    add round number/game over setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,6 +49,7 @@ var fight = function(enemyName) {
 
             // award player money for winning
             playerMoney = playerMoney + 20;
+            console.log("playerMoney", playerMoney);
 
             // leave while() loop since enemy is dead
             break;
@@ -78,10 +79,27 @@ var fight = function(enemyName) {
     }
 };
 
-
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // call fight with the enemy-robot
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have +1 added to it
+        window.alert("Welcome Robot Gladiators! Round " + (i + 1) );
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        //use debugger to pause script from running and check what's going on at the moment in the code
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+
+    }
+
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
