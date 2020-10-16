4261f5672ae9b7244c9d432a4f9c3664a6d7b712
MyBoyDenis <shelbykohring@Shelbys-MacBook-Pro.local>
2020-10-15 23:24:10 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -46,14 +46,16 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye.");
                 //subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
     
-        // remove enemy's health by subtracting the amount set in the player attack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack -3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
 
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -75,8 +77,10 @@ var fight = function(enemyName) {
                 window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
             }
 
-        // remove the players health by subtracting the amount set in the enemy attack variable
-        playerHealth = playerHealth - enemyAttack;
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        
+        playerHealth = Math.max(0, playerHealth - damage);
 
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -108,7 +112,7 @@ var startGame = function() {
 
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             fight(pickedEnemyName);
 
@@ -157,6 +161,7 @@ var endGame = function() {
     }
 };
 
+// shop function
 var shop = function() {
     // ask the player what they'd like to do
     var shopOptionPrompt = window.prompt(
@@ -210,6 +215,13 @@ var shop = function() {
             shop();
             break;
     } 
+};
+
+// random number value function
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (min - max + 1) + min);
+
+    return value;
 };
 
 // startGame() function call
